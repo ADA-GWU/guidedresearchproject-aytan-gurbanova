06b5b7c728b783c4275005662d8d0361a6613dab
aytan-gurbanova <aytangurbanova0@gmail.com>
2023-06-11 21:59:50 +0400

    skeleton of monolith application is added

---
+++ code/monolith/ecomm-mono/src/main/java/ada/research/ecommmono/EcommMonoApplication.java
@@ -0,0 +1,13 @@
+package ada.research.ecommmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommMonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommMonoApplication.class, args);
+    }
+
+}
---
+++ code/monolith/ecomm-mono/src/test/java/ada/research/ecommmono/EcommMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package ada.research.ecommmono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommMonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
