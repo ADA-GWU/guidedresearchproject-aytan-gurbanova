47c07c457df8b3da59eda090dc9d8ef34aab007a
aytan-gurbanova <aytangurbanova0@gmail.com>
2023-06-12 16:32:18 +0400

    security basics added

--- code/monolith/ecomm-mono/src/main/java/ada/research/ecommmono/config/SecurityConfig.java
+++ code/monolith/ecomm-mono/src/main/java/ada/research/ecommmono/config/SecurityConfig.java
@@ -1,4 +1,40 @@
 package ada.research.ecommmono.config;
 
+import ada.research.ecommmono.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(authConfig -> {
+            authConfig.requestMatchers("/ecomm/registration/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated();
+        }).formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(userService);
+        return provider;
+    }
 }
