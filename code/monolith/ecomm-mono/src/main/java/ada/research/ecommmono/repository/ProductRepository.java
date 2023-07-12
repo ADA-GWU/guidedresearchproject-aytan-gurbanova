d5082f83192b6e04ea609e6b3d0dd1631a7ae132
aytan-gurbanova <aytangurbanova0@gmail.com>
2023-07-12 08:15:50 -0400

    Midterm report added

--- code/monolith/ecomm-mono/src/main/java/ada/research/ecommmono/repository/ProductRepository.java
+++ code/monolith/ecomm-mono/src/main/java/ada/research/ecommmono/repository/ProductRepository.java
@@ -8,22 +8,24 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-        @Query("SELECT p FROM Product p WHERE (:brand IS NULL OR p.brand = :brand) " +
-                "AND (:productName IS NULL OR p.name = :productName) " +
-                "AND (:categoryName IS NULL OR p.category.name = :categoryName) " +
-                "AND (:priceMin IS NULL OR p.price >= :priceMin) " +
-                "AND (:priceMax IS NULL OR p.price <= :priceMax)")
-        List<Product> filterProducts(
+    @Query("SELECT p FROM Product p WHERE " +
+            "(:brand IS NULL OR p.brand = :brand) " +
+            "AND (:productName IS NULL OR p.name = :productName) " +
+            "AND (:categoryName IS NULL OR p.category.name = :categoryName) " +
+            "AND (:priceMin IS NULL OR p.price >= :priceMin) " +
+            "AND (:priceMax IS NULL OR p.price <= :priceMax) " +
+            "ORDER BY p.id")
+    List<Product> filterProducts(
                 String brand,
                 String productName,
                 String categoryName,
                 Float priceMin,
-                Float priceMax
-    );
-        @Query("SELECT p FROM Product p WHERE " +
+                Float priceMax);
+
+    @Query("SELECT p FROM Product p WHERE " +
                 "LOWER(p.brand) LIKE CONCAT('%', LOWER(:keyword), '%') OR " +
                 "LOWER(p.name) LIKE CONCAT('%', LOWER(:keyword), '%') OR " +
                 "LOWER(p.description) LIKE CONCAT('%', LOWER(:keyword), '%') OR " +
                 "LOWER(p.category.name) LIKE CONCAT('%', LOWER(:keyword), '%')")
-        List<Product> searchProduct(String keyword);
+    List<Product> searchProduct(String keyword);
 }
